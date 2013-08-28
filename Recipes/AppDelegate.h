c16de37300f9b08399e293d3e4d88fe1e8df80f7
Sam Coward <idoru42@gmail.com>
2013-08-28 12:46:31 -0400

    Initial Commit

---
+++ Recipes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Recipes
+//
+//  Created by Sam Coward on 8/28/13.
+//  Copyright (c) 2013 Sam Coward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
